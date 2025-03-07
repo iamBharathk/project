57e8475d948539fe72c203543f93b52946542968
Sagarchikkalli123 <ksbhanavi@gmail.com>
2025-03-07 06:55:47 +0530

    First pgm

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 10,b = 20;
+        int c = a + b;
+        int d = a + b;
+        System.out.println(d);
+        System.out.println(c);
+        }
+    }
